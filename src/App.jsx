6735ce11dd8e54e079cd566d10f41bdf8365a9ec
Amit Mallick <amitmallick837@gmail.com>
2026-01-19 18:15:56 +0530

    removed landing page

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { ChatProvider } from "./context/ChatContext";
 import { ThemeProvider } from "./context/ThemeContext"; // 🔥 1. Import ThemeProvider
 
 // Pages
-import LandingPage from "./pages/LandingPage";
+//import LandingPage from "./pages/LandingPage";
 import Signup from "./pages/Signup";
 import Chat from "./pages/app_pages/Chat";
 import ChatWindow from "./pages/app_pages/ChatWindow";
@@ -32,9 +32,9 @@ const App = () => {
       <div className="App">
         <ChatProvider>
           <Routes>
-            {/* Auth Route */}
-            <Route path="/" element={<LandingPage />} />
-            <Route path="/signup_login" element={<Signup />} />
+            {/* Auth Route 
+            <Route path="/" element={<LandingPage />} />*/}
+            <Route path="/" element={<Signup />} />
 
             {/* Main App Routes (With BottomNav) */}
             <Route path="/app" element={<Chat />} />

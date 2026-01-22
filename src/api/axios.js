2b4a1936fb20d4a720aa8b249c7635b198f5b2e8
Amit Mallick <amitmallick837@gmail.com>
2026-01-22 11:58:01 +0530

    changed backend url

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://split-sync-backend-1.onrender.com",
+  baseURL: "https://split-sync-backend-2.onrender.com",
   withCredentials: true,
 });
 
